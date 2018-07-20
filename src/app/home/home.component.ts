38e8c6513ae29719460583649cf27861f5a4caf9
David Lucero <david.lucero@ttu.edu>
2018-07-19 19:04:14 -0500

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
   }
 
 }
