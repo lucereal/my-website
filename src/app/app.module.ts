9311b60fdee9e776ed3e16388fda8f49d8d5182e
David Lucero <david.lucero@ttu.edu>
2018-09-17 09:38:52 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'resume', component:ResumeComponent}
+  {path: 'resume', component:ResumeComponent},
+  {path: 'projects', component: ProjectsComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ResumeComponent
+    ResumeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
